0ba2cbdf788d91ff4cae48a4325276b78a0d3aa0
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-16 07:50:44 +0700

    create testcase.py for comparing answers of random testcase with other

---
+++ Lab/test/testcase.py
@@ -0,0 +1,19 @@
+jml_query = int(input("jumlah perintah: "))
+ans1 = []
+ans2 = []
+
+for i in range(jml_query):
+    answer = int(input())
+    ans1.append(answer)
+for i in range(jml_query):
+    answer = int(input())
+    ans2.append(answer)
+
+matches = 0
+for i in range(jml_query):
+    if ans1[i] == ans2[i]:
+        matches += 1
+        print(f"{ans1[i]:<3}||{ans2[i]:>3}     ===     {ans1[i] == ans2[i]}")
+
+print()
+print(f"matches: {matches}/{jml_query} ({matches/jml_query*100}%)")
