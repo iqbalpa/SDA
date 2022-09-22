061985df7f7fa4167fc53c7b0ca54c08cac0b37e
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-22 23:55:09 +0700

    create test case generate for lab 3

--- Lab/test/lab3.py
+++ Lab/test/lab3.py
@@ -3,7 +3,7 @@ import random
 N = int(input("Jumlah Pulau: "))
 print(N)
 for i in range(N):
-    angka = random.randint(1, 2)
+    angka = random.randint(1, 5)
     if angka == 1:
         print("R", end=" ")
     else:
---
+++ TP/TP 1/TP1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class TP1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+
+        input.close();
+    }
+}
