642f7d7907c3e89cd11f31fd342dda60f51e9709
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-28 08:14:27 +0700

    TP 1: implement P and L function

--- TP/TP 1/TP1.java
+++ TP/TP 1/TP1.java
@@ -1,12 +1,17 @@
 import java.util.*;
 
 public class TP1 {
+    static Menu[] arrMenu;
+    static Koki[] arrKoki;
+    static Pelanggan[] arrPelanggan;
+    static Queue<Menu> pesanan = new LinkedList<>();
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         // banyak menu
         int M = input.nextInt();
-        Menu[] arrMenu = new Menu[M];
+        arrMenu = new Menu[M];
         for (int i = 0; i < M; i++) {
             int harga = input.nextInt();
             String tipe = input.next();
@@ -16,7 +21,7 @@ public class TP1 {
         
         // banyak koki
         int V = input.nextInt();
-        Koki[] arrKoki = new Koki[V];
+        arrKoki = new Koki[V];
         for (int i = 0; i < V; i++) {
             String spesialisasi =  input.next();
             Koki newKoki = new Koki(spesialisasi);
@@ -31,31 +36,44 @@ public class TP1 {
         int Y = input.nextInt();
 
         // array pelanggan
-        Pelanggan[] arrPelanggan = new Pelanggan[P];
+        arrPelanggan = new Pelanggan[P];
         // jumlah pelanggan per hari
-        int[] pelangganHariKe = new int[Y];
+        int[] PelangganPerHari = new int[Y];
         // jumlah pelayanan per hari
-        int[] pelayananHariKe = new int[Y];
+        int[] pelayananPerHari = new int[Y];
 
         for (int i = 0; i < Y; i++) {
-            pelangganHariKe[i] = input.nextInt();
-            for (int j=0; j<pelangganHariKe[i]; j++){
+            PelangganPerHari[i] = input.nextInt();
+            for (int j=0; j<PelangganPerHari[i]; j++){
                 int I = input.nextInt();
                 char K = input.next().charAt(0);
                 int U = input.nextInt();
                 Pelanggan newPelanggan;
                 if (K == '?'){
                     int R = input.nextInt();
+
+                    // advance scanning
+                    int negatif = 0;
+                    int positif = 0;
+                    for (int k=I-R; k<=I-1; k++){
+                        if (arrPelanggan[k].K == '-'){
+                            negatif++;
+                        } else {
+                            positif++;
+                        }
+                        K = negatif < positif ? '+' : '-';
+                    }
                     newPelanggan = new Pelanggan(I, K, U, R);
+
                 } else {
                     newPelanggan = new Pelanggan(I, K, U);
                 }
                 arrPelanggan[I] = newPelanggan;
             }
 
-            pelayananHariKe[i] = input.nextInt();
+            pelayananPerHari[i] = input.nextInt();
             // fungsi pelayanan
-            for (int j=0; j<pelayananHariKe[i]; j++){
+            for (int j=0; j<pelayananPerHari[i]; j++){
                 int satu;
                 int dua;
                 int tiga;
@@ -78,8 +96,33 @@ public class TP1 {
         input.close();
     }
 
-    public void P(int idPelanggan, int indexMakanan){}
-    public void L(){}
+    public void P(int idPelanggan, int indexMakanan){
+        Koki theKoki = arrKoki[0];
+        Menu theMenu = arrMenu[indexMakanan];
+        int indexKoki = 0;
+        for (int i=0; i<arrKoki.length; i++){
+            if (arrKoki[i].spesialisasi.equals(theMenu.tipe)){
+                if (!theKoki.spesialisasi.equals(theMenu.tipe)){
+                    theKoki = arrKoki[i];
+                    indexKoki = i;
+                } else if (theKoki.jumlahMelayani > arrKoki[i].jumlahMelayani){
+                    theKoki = arrKoki[i];
+                    indexKoki = i;
+                } else if (theKoki.jumlahMelayani == arrKoki[i].jumlahMelayani && i > indexKoki) {
+                    theKoki = arrKoki[indexKoki];
+                }
+            }
+        }
+        pesanan.add(theMenu);
+        theKoki.jumlahMelayani++;
+    }
+    public void L(){
+        for (Menu m: pesanan){
+            m.koki.jumlahMelayani--;
+            m.koki.jumlahPelayanan++;
+            pesanan.remove();
+        }
+    }
     public void B(int idPelanggan){}
     public void C(int Q){}
     public void D(int A, int G, int S){}
@@ -88,6 +131,7 @@ public class TP1 {
 class Menu {
     int harga;
     String tipe;
+    Koki koki;
 
     public Menu(int harga, String tipe) {
         this.harga = harga;
@@ -117,9 +161,11 @@ class Pelanggan {
 class Koki {
     String spesialisasi;
     int jumlahPelayanan;
+    int jumlahMelayani;
 
     public Koki(String spesialisasi){
         this.spesialisasi = spesialisasi;
         this.jumlahPelayanan = 0;
+        this.jumlahMelayani = 0;
     }
 }
