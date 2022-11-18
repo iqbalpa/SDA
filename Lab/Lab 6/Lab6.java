b180b711207edadf134ed522e16ac3d557e9ba8f
iqbalpa <iqbalpahlevi16@gmail.com>
2022-11-18 15:30:40 +0700

    Lab 6: implement query TAMBAH #1

--- Lab/Lab 6/Lab6.java
+++ Lab/Lab 6/Lab6.java
@@ -5,54 +5,82 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
-import java.util.Vector;
+import java.util.*;
 
 public class Lab6 {
     private static InputReader in;
     static PrintWriter out;
-    // static BinaryHeap binaryheap;
-    static MyHeap myheap;
+    static Saham[] daftarSaham = new Saham[400012];
+    static MaxHeap maxheap;
+    static MinHeap minheap;
+    static int N;
 
     public static void main(String[] args) {
         InputStream inputStream = System.in;
         in = new InputReader(inputStream);
         OutputStream outputStream = System.out;
         out = new PrintWriter(outputStream);
-
+ 
         // banyak saham
-        int N = in.nextInt();
-        // binaryheap = new BinaryHeap(N);
-        myheap = new MyHeap(N);
+        N = in.nextInt();
         for (int i=1; i<=N; i++) {
             int Ci = in.nextInt();
             Saham newSaham = new Saham(i, Ci);
-            myheap.insert(newSaham);
+            daftarSaham[i] = newSaham;
         }
+
+        Arrays.sort(daftarSaham, 1, N+1);
+        int batasAtasMaxHeap = -999;
+        int batasBawahMinHeap = -999;
+        if (N%2 == 0){
+            batasAtasMaxHeap = N/2 - 1;
+            batasBawahMinHeap = N/2 + 1;
+        } else {
+            batasAtasMaxHeap = N/2;
+            batasBawahMinHeap = N/2 + 1;
+        }
+        maxheap = new MaxHeap();
+        for (int i=1; i<=batasAtasMaxHeap; i++) {
+            maxheap.insert(daftarSaham[i]);
+        }
+        minheap = new MinHeap();
+        for (int i=batasBawahMinHeap; i<=N; i++) {
+            minheap.insert(daftarSaham[i]);
+        }
+
         // banyak query
         int Q = in.nextInt();
         for (int i=0; i<Q; i++){
             String query = in.next();
             if (query.equals("TAMBAH")){
                 int C = in.nextInt();
-                tambah(C);
+                TAMBAH(C);
             }
             else if (query.equals("UBAH")){
                 int X = in.nextInt();
                 int C = in.nextInt();
-                ubah(X, C);
+                UBAH(X, C);
             }
         }
-
         out.close();
     }
-
-    static void tambah(int C){
-        Saham newSaham = new Saham(myheap.getSize()+1, C);
-        myheap.insert(newSaham);
+    static void TAMBAH(int C){
+        N++;
+        Saham newSaham = new Saham(N, C);
+        Saham minSaham = minheap.getMin();
+        if (newSaham.compareTo(minSaham) < 0){
+            // System.out.println(">>>>> tail: " + maxheap.tail);
+            maxheap.insert(newSaham);
+            // System.out.println(">>>>> new tail: " + maxheap.tail);
+        } else {
+            // System.out.println(">>>>> tail: " + minheap.tail);
+            minheap.insert(newSaham);
+            // System.out.println(">>>>> new tail: " + minheap.tail);
+        }
+        minSaham = minheap.getMin();
+        out.println(minSaham.seri);
     }
-    static void ubah(int X, int C){
-        myheap.change(X, C);
-    }
+    static void UBAH(int X, int C){}
 
     // taken from https://codeforces.com/submissions/Petr
     static class InputReader {
@@ -82,158 +110,112 @@ public class Lab6 {
 }
 
 
-class Saham {
+class Saham implements Comparable<Saham> {
     int seri, harga;
     Saham (int seri, int harga){
         this.seri = seri;
         this.harga = harga;
     }
-}
-// PPT
-class MyHeap {
-    Vector<Saham> data;
-    MyHeap(int N){
-        data = new Vector<>(N);
+    public int compareTo(Saham o){
+        if (this.harga < o.harga) return -1;
+        else if (this.harga > o.harga) return 1;
+        else return this.seri - o.seri;
     }
-    int parentOf(int i){
+}
+
+// * 1/2 TERMAHAL
+class MinHeap {
+    Saham[] heap;
+    int tail;
+    MinHeap(){
+        this.tail = 0;
+        heap = new Saham[200012];
+    }
+    int parent(int i){
         return (i-1)/2;
     }
-    int leftChildOf(int i){
+    int leftChild(int i){
         return 2*i+1;
     }
-    int rightChildOf(int i){
-        return 2*(i+1);
+    int rightChild(int i){
+        return 2*i+2;
     }
+    // get saham paling murah di 1/2 termahal
     Saham getMin(){
-        return data.get(0);
+        return heap[0];
     }
-    void percolateDown(int root){
-        int heapSize = data.size();
-        Saham value = data.get(root);
-        while (root < heapSize){
-            int childPos = leftChildOf(root);
-            if (childPos < heapSize){
-                if ((rightChildOf(root) < heapSize) && (data.get(childPos).harga > data.get(childPos+1).harga)){
-                    childPos++;
-                }
-                if (value.harga > data.get(childPos).harga){
-                    data.set(root, data.get(childPos));
-                    root = childPos;
-                }
-                else {
-                    data.set(root, value);
-                    return;
-                }
-            }
-            else {
-                data.set(root, value);
-                return;
-            }
+    void swap(int i, int j){
+        Saham temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+    void percolateUp(int i){
+        if (i==0) return;
+        if (heap[parent(i)].compareTo(heap[i]) > 0){
+            swap(i, parent(i));
+            percolateUp(parent(i));
         }
     }
-    void percolateUp(int leaf){
-        int parent = parentOf(leaf);
-        Saham value = data.get(leaf);
-        while (leaf > 0 && value.harga < data.get(parent).harga){
-            data.set(leaf, data.get(parent));
-            leaf = parent;
-            parent = parentOf(leaf);
-        }
-        data.set(leaf, value);
-    }
-    boolean isKosong() {
-        return data.size() == 0;
-    }
-    int getSize(){
-        return data.size();
-    }
-    void clear(){
-        data.clear();
-    }
-    public String toString(){
-        return data.toString();
-    }
-    // create heapify method using percolateDown and percolateUp
-    void heapify(){
-        int heapSize = data.size();
-        for (int i=parentOf(heapSize-1); i>=0; i--){
-            percolateDown(i);
+    void percolateDown(int i){
+        int min = i;
+        if (leftChild(i) < tail && heap[leftChild(i)].compareTo(heap[min]) < 0) min = leftChild(i);
+        if (rightChild(i) < tail && heap[rightChild(i)].compareTo(heap[min]) < 0) min = rightChild(i);
+        if (min != i){
+            swap(i, min);
+            percolateDown(min);
         }
     }
-    void insert(Saham value){
-        data.add(value);
-        percolateUp(data.size()-1);
-    }
-    Saham removeMin(){
-        Saham min = data.get(0);
-        data.set(0, data.get(data.size()-1));
-        data.remove(data.size()-1);
-        percolateDown(0);
-        return min;
-    }
-    void remove(int i){
-        data.set(i, data.get(data.size()-1));
-        data.remove(data.size()-1);
-        percolateDown(i);
-    }
-    void change(int X, int C){
-        Saham theSaham = getSaham(X);
-        theSaham.harga = C;
-        percolateDown(X-1);
-        percolateUp(X-1);
-    }
-    Saham getSaham(int seri){
-        for (int i=0; i<data.size(); i++){
-            if (data.get(i).seri == seri){
-                return data.get(i);
-            }
-        }
-        return null; 
-    }
-    void print(){
-        for (int i=0; i<data.size(); i++){
-            System.out.print(data.get(i).harga + " ");
-        }
-        System.out.println();
+    void insert(Saham saham){
+        heap[tail] = saham;
+        tail++;
+        percolateUp(tail-1); //percolate up newest element
     }
 }
-// GFG
-// class BinaryHeap {
-//     Saham[] heap;
-//     int size;
-//     int maxsize;
-//     BinaryHeap(int maxsize){
-//         this.maxsize = maxsize;
-//         this.size = 0;
-//         heap = new Saham[this.maxsize + 1];
-//         heap[0] = new Saham(-999, -999);
-//     }
-//     int parent(int i){
-//         return i/2;
-//     }
-//     int leftChild(int i){
-//         return 2*i;
-//     }
-//     int rightChild(int i){
-//         return 2*i+1;
-//     }
-//     boolean isLeaf(int i){
-//         if (i > (this.size/2)){
-//             return true;
-//         }
-//         return false;
-//     }
-//     void swap(int i, int j){
-//         Saham temp = heap[i];
-//         heap[i] = heap[j];
-//         heap[j] = temp;
-//     }
-//     void insert(Saham saham){
-//         heap[++size] = saham;
-//         int current = size;
-//         while (heap[current].harga < heap[parent(current)].harga){
-//             swap(current, parent(current));
-//             current = parent(current);
-//         }
-//     }
-// }
+// * 1/2 TERMURAH
+class MaxHeap {
+    Saham[] heap;
+    int tail;
+    MaxHeap(){
+        this.tail = 0;
+        heap = new Saham[200012];
+    }
+    int parent(int i){
+        return (i-1)/2;
+    }
+    int leftChild(int i){
+        return 2*i+1;
+    }
+    int rightChild(int i){
+        return 2*i+2;
+    }
+    // get saham paling mahal di 1/2 termurah
+    Saham getMax(){
+        return heap[0];
+    }
+    void swap(int i, int j){
+        Saham temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+    void percolateUp(int i){
+        if (i==0) return;
+        if (heap[parent(i)].compareTo(heap[i]) < 0){
+            swap(i, parent(i));
+            percolateUp(parent(i));
+        }
+    }
+    void percolateDown(int i){
+        int max = i;
+        if (leftChild(i) < tail && heap[leftChild(i)].compareTo(heap[max]) > 0) max = leftChild(i);
+        if (rightChild(i) < tail && heap[rightChild(i)].compareTo(heap[max]) > 0) max = rightChild(i);
+        if (max != i){
+            swap(i, max);
+            percolateDown(max);
+        }
+    }
+    void insert(Saham saham){
+        heap[tail] = saham;
+        tail++;
+        percolateUp(tail-1); // percolate up newest element
+    }
+}
