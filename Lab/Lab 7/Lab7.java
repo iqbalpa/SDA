748e81b633e6cae01a4378b327563d30949b0e06
iqbalpa <iqbalpahlevi16@gmail.com>
2022-11-25 17:09:12 +0700

    Lab 7: GFG #1

--- Lab/Lab 7/Lab7.java
+++ Lab/Lab 7/Lab7.java
@@ -1,11 +1,15 @@
 import java.io.*;
 import java.util.*;
 
+// IDEAS BY:
+// 1. Fresty Tania S
+
 public class Lab7 {
     private static InputReader in;
     private static PrintWriter out;
     static List<Integer> attackedBenteng = new ArrayList<>();
     static Integer[] distances;
+    static ArrayList<ArrayList<AdjListNode>> graf = new ArrayList<>();
 
     public static void main(String[] args) {
         InputStream inputStream = System.in;
@@ -16,28 +20,50 @@ public class Lab7 {
         int N = in.nextInt(), M = in.nextInt();
         distances = new Integer[N];
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             // TODO: Inisialisasi setiap benteng
-            
+            graf.add(new ArrayList<>());
+            distances[i] = Integer.MAX_VALUE;
         }
 
         for (int i = 0; i < M; i++) {
             int F = in.nextInt();
             // TODO: Tandai benteng F sebagai benteng diserang
-            
+            attackedBenteng.add(F-1);
         }
 
         int E = in.nextInt();
         for (int i = 0; i < E; i++) {
             int A = in.nextInt(), B = in.nextInt(), W = in.nextInt();
             // TODO: Inisialisasi jalan berarah dari benteng A ke B dengan W musuh
-            
+            graf.get(A-1).add(new AdjListNode(B-1, W));
         }
+
+        // looping over attackedBenteng
+        for (int i = 0; i < attackedBenteng.size(); i++) {
+            int[] temp = dijkstra(N, graf, attackedBenteng.get(i));
+            for (int j = 0; j < temp.length; j++) {
+                if (distances[j] > temp[j]) {
+                    distances[j] = temp[j];
+                }
+            }
+        }
+
+        out.println("sortest path to attacked benteng: ");
+        for (int i = 0; i < distances.length; i++) {
+            out.println(distances[i] + " ");
+        }
+        out.println();
 
         int Q = in.nextInt();
         while (Q-- > 0) {
             int S = in.nextInt(), K = in.nextInt();
             // TODO: Implementasi query
+            if (distances[S-1] < K){
+                out.println("YES");
+            } else {
+                out.println("NO");
+            }
         }
         out.close();
     }
@@ -71,5 +97,41 @@ public class Lab7 {
             return Integer.parseInt(next());
         }
 
+    }
+
+
+    // REFERENSI: Geeksforgeeks https://www.geeksforgeeks.org/dijkstras-algorithm-for-adjacency-list-representation-greedy-algo-8/
+    static class AdjListNode {
+        int vertex, weight;
+        AdjListNode(int v, int w){
+            this.vertex = v;
+            this.weight = w;
+        }
+        int getVertex(){
+            return this.vertex;
+        }
+        int getWeight(){
+            return this.weight;
+        }
+    }
+    public static int[] dijkstra(int V, ArrayList<ArrayList<AdjListNode>> graph, int src){
+        int[] distance = new int[V];
+        for (int i=0; i<V; i++) distance[i] = Integer.MAX_VALUE;
+        distance[src] = 0;
+
+        PriorityQueue<AdjListNode> pq = new PriorityQueue<>((v1,v2) -> v1.getWeight() - v2.getWeight());
+        pq.add(new AdjListNode(src, 0));
+
+        while (pq.size() > 0){
+            AdjListNode current = pq.poll();
+            
+            for (AdjListNode n: graph.get(current.getVertex())){
+                if (distance[current.getVertex()] + n.getWeight() < distance[n.getVertex()]){
+                    distance[n.getVertex()] = distance[current.getVertex()] + n.getWeight();
+                    pq.add(new AdjListNode(n.getVertex(), distance[n.getVertex()]));
+                }
+            }
+        }
+        return distance;
     }
 }
